ef1cfd797ef3aad6ecf9b884f94a69b7e3a608e9
JonyNhoque <jgabriel.nhoque@gmail.com>
2022-03-07 23:40:50 -0300

    Criando repo e primeiro projeto wp

---
+++ index.js
@@ -0,0 +1,6 @@
+let tiliDad = 'João';
+let tiliMom = 'Carol';
+let tiliBaby = 'Zitos';
+
+let tiliFamily = 'O ' + tiliDad + '' + ' ama demais, demais, demaaaaais a ' + tiliMom + ' e o '+ tiliBaby;
+console.log(tiliFamily)
